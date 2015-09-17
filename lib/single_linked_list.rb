3e5565487664be0b5c9d2a4b698070ffccfb1a83
minustehbare <minustehbare@gmail.com>
2015-09-16 22:31:25 -0400

    Remove whitespace

--- lib/single_linked_list.rb
+++ lib/single_linked_list.rb
@@ -5,7 +5,7 @@ class SingleLinkedList
   def tail
     return self.reduce(nil) { |tail, node| node }
   end
-  
+
   def append(new_node)
     tail_node = self.tail
     if tail_node.nil?
@@ -14,12 +14,12 @@ class SingleLinkedList
       tail_node.pointer = new_node
     end
   end
-  
+
   def prepend(new_node)
     new_node.pointer = @head
     @head = new_node
   end
-  
+
   def find_by_data(data)
     self.find { |node| node.data == data }
   end
@@ -28,25 +28,25 @@ class SingleLinkedList
     self.each_with_index { |node, i| return node if i == x }
     return nil
   end
-  
+
   def each
     return nil unless @head
-    
+
     node = @head
     while node do
       yield(node)
       node = node.pointer
     end
   end
-  
+
   def inspect
     return "[#{self.entries.map { |node| node.data }.join("->")}]"
   end
-  
+
   class Node
     attr_accessor :pointer
     attr_reader :data
-    
+
     def initialize(data = nil)
       data = Random.rand(1..99) unless data
       @data = data
--- spec/single_linked_list_spec.rb
+++ spec/single_linked_list_spec.rb
@@ -6,10 +6,10 @@ describe SingleLinkedList do
   let(:tail) { list.tail }
   let(:count) { list.count }
   let(:inspect) { list.inspect }
-  
+
   shared_examples "a populated list" do
     before { list.prepend(SingleLinkedList::Node.new) }
-    
+
     describe "the head node" do
       subject { head }
       it_behaves_like "a list node"
@@ -22,13 +22,13 @@ describe SingleLinkedList do
     it { is_expected.not_to be_nil }
     it { is_expected.to be_an_instance_of(SingleLinkedList::Node) }
   end
-  
+
   context "when the list is empty" do
     describe "the head node" do
       subject { head }
       it { is_expected.to be_nil }
     end
-    
+
     describe "the tail node" do
       subject { tail }
       it { is_expected.to be_nil }
@@ -43,14 +43,14 @@ describe SingleLinkedList do
 
   context "when the list has exactly one node" do
     before { list.prepend(SingleLinkedList::Node.new) }
-    
+
     it_behaves_like "a populated list"
 
     describe "the head node's pointer" do
       subject { head.pointer }
       it { is_expected.to be_nil }
     end
-    
+
     describe "the tail node" do
       it "is the same as the head" do
         expect(tail).to equal(head)
@@ -63,11 +63,11 @@ describe SingleLinkedList do
       end
     end
   end
-  
+
   context "when the list has two nodes" do
     before { 2.times { list.prepend(SingleLinkedList::Node.new) } }
     it_behaves_like "a populated list"
-    
+
     describe "the head node's pointer" do
       subject(:pointer) { head.pointer }
       it { is_expected.not_to be_nil }
@@ -84,14 +84,14 @@ describe SingleLinkedList do
         expect(tail).not_to equal(head)
       end
     end
-    
+
     describe "#inspect" do
       it "returns a list representation with the head data, a pointer, and the tail data" do
         expect(inspect).to eql("[#{head.data}->#{tail.data}]")
       end
     end
   end
-  
+
   context "when the list has three or more nodes" do
     before { 10.times { list.prepend(SingleLinkedList::Node.new) } }
     it_behaves_like "a populated list"
@@ -104,7 +104,7 @@ describe SingleLinkedList do
         expect(tail).not_to equal(head)
       end
     end
-   
+
     describe "the head node's pointer" do
       it "does not point to the tail" do
         expect(head.pointer).not_to equal(tail)
@@ -121,12 +121,12 @@ describe SingleLinkedList do
       end
     end
   end
-  
+
   context "when adding a node to the list" do
     let(:new_node) { SingleLinkedList::Node.new }
     describe "using #append" do
       before { list.append(new_node) }
-      
+
       it "adds a node to the end of the list" do
         expect(new_node.pointer).to be_nil
         expect(new_node).to equal(tail)
@@ -139,38 +139,38 @@ describe SingleLinkedList do
         expect(new_node).to equal(head)
       end
     end
-    
+
     describe "using #insert_after" do
       it "adds the node after the specified node" do
       end
     end
   end
-  
+
   context "when removing a node from the list" do
     describe "using #remove_after" do
       it "remove the node after the specified node" do
       end
     end
   end
-  
+
   context "when finding a node" do
     before {
       10.times { |x| list.append(SingleLinkedList::Node.new(x + 1)) }
       list.append(SingleLinkedList::Node.new(1))
       list.prepend(SingleLinkedList::Node.new(10))
     }
-    
+
     describe "using #find_by_data" do
       it "returns the first node found that contains the specified data" do
         expect(list.find_by_data(10)).to equal(head)
         expect(list.find_by_data(1)).not_to equal(tail)
       end
-      
+
       it "returns nil if no node contains the specified data" do
         expect(list.find_by_data(20)).to be_nil
       end
     end
-    
+
     describe "using #find_by_index" do
       context "when index is out of range" do
         it "returns nil" do
@@ -178,7 +178,7 @@ describe SingleLinkedList do
           expect(list.find_by_index(1000)).to be_nil
         end
       end
-      
+
       context "when index is in range" do
         it "retrieves the n-th node in the list" do
           expect(list.find_by_index(1)).not_to be_nil
@@ -204,8 +204,8 @@ describe SingleLinkedList::Node do
   describe "#data" do
     subject { node.data }
     it { is_expected.not_to be_nil }
-  end  
-  
+  end
+
   describe "#inspect" do
     it "returns the node data, a pointer, and the pointer's data" do
       expect(node.inspect).to eql("#{data}->#{pointer.inspect}")
